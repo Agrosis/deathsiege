3100df8ef616dae5f9076c68ffa5d8361f7a6585
agro <agro@jantox.com>
2013-06-18 01:36:45 -0500

    Fixed add sniper.

---
+++ src/com/jantox/siege/entities/tools/Sniper.java
@@ -0,0 +1,99 @@
+package com.jantox.siege.entities.tools;
+
+import com.jantox.siege.GameInstance;
+import com.jantox.siege.Input;
+import com.jantox.siege.Resources;
+import com.jantox.siege.Vector3D;
+import com.jantox.siege.entities.Entity;
+import com.jantox.siege.entities.Player;
+
+import java.util.ArrayList;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.glEnable;
+
+public class Sniper extends Tool {
+
+    private Player powner;
+
+    private boolean use = false;
+    private boolean scoped = false;
+
+    private Vector3D sp;
+    private float slen,olen;
+    private float scope;
+
+    public Sniper(Player owner) {
+        super(owner);
+        this.powner = owner;
+
+        sp = new Vector3D(-2, -1,-0.8);
+        slen = olen = (float)sp.length();
+        sp.normalize();
+    }
+
+    boolean r = false;
+
+    public void update(float delta) {
+        if(Input.rmouse && r == false) {
+            r = true;
+            if(GameInstance.sniper) {
+                GameInstance.sniper = false;
+            } else {
+                GameInstance.sniper = true;
+            }
+        }
+
+        if(!Input.rmouse) {
+            r = false;
+        }
+    }
+
+    @Override
+    public void onUse(int mouse) {
+        use = true;
+        if(mouse == 0) {
+            level.addProjectile(new Projectile(Projectile.SNIPER, powner.getCamera().getHoldingPosition(), powner.getCamera().getDirectionVector()));
+
+            GameInstance.audio.playSound(0);
+        }
+    }
+
+    @Override
+    public void onRelease() {
+        use = false;
+    }
+
+    @Override
+    public int getRest() {
+        return 45;
+    }
+
+    @Override
+    public void render() {
+        glDisable(GL_DEPTH_TEST);
+        glEnable(GL_TEXTURE_2D);
+        glPushMatrix();
+        Vector3D hold = powner.getCamera().getHoldingPosition();
+        glTranslatef((float) hold.x, (float) hold.y, (float) hold.z);
+        glRotatef(powner.getCamera().getYaw() - 90, 0, 1, 0);
+        glRotatef(powner.getCamera().getPitch(), 0, 0, 1);
+
+
+        glScalef(1.5f, 1.5f, 1.5f);
+
+        //sp.multiply(slen);
+        glTranslatef((float)sp.x, (float)sp.y, (float)sp.z);
+        //sp.normalize();
+        /*glScalef(1.5f, 1.5f, 1.5f);
+
+        glColor3f(0.25f,0.25f,0.25f);*/
+
+        if(!GameInstance.sniper)
+            glCallList(Resources.getModel(12));
+        glPopMatrix();
+        glEnable(GL_DEPTH_TEST);
+    }
+}
+
